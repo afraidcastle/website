7514a96aa05d688223dfe9bb23802fc26c5fcd59
Addison_Glenn <addisonglenn92@gmail.com>
2026-06-22 16:43:52 +0800

    Update main page
    Changed the layout of the main page as well as decorated the site a little

--- blogs/script.js
+++ blogs/script.js
@@ -6,17 +6,17 @@ document.addEventListener("DOMContentLoaded", () => {
   dropdown.addEventListener("change", function() {
 	  const selectedValue = dropdown.value;
 		if (selectedValue == "me")
-			{
-				displayText.textContent = "Right now I am waiting for my placement in university, so nothing much to do but build this website, eat, sleep and repeat. Although, maybe I should exercise more though.";
-				displayHeader.textContent = "Here are some stuff about me";
-			}
+		{
+			displayHeader.textContent = "Here are some stuff about me";
+			displayText.innerHTML = "Right now I am waiting for my placement in university, so nothing much to do but build this website, eat, sleep and repeat. Although, maybe I should exercise more though.";
+		}
 		else if (selectedValue == "game"){
-				displayText.textContent = "Games I play a lot: Terraria. That's all you get for now.";
-				displayHeader.textContent = "These are games I play";
-			}
+			displayHeader.textContent = "These are games I play";
+			displayText.innerHTML = "Games I play a lot: Terraria. That's all you get for now.";
+		}
 		else if (selectedValue == "food") {
-				displayText.textContent = "I am a bit shy to admit I've eaten way more than what my body could handle (I weigh 53 kilos as of writing).";
-				displayHeader.textContent = "I love eating food";
-			}
+			displayHeader.textContent = "I love eating food";
+			displayText.innerHTML = "I am a bit shy to admit I've eaten way more than what my body could handle (I weigh 53 kilos as of writing).";
+		}
 	});
 });
